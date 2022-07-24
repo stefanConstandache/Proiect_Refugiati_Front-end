ea5b3b3bda311fdf0582fb58aad86a0027cdda34
stefanConstandache <stefan.m.constandache@gmail.com>
2022-07-24 20:34:01 +0300

    Update newsapiservice.service.ts

--- src/app/services/newsapiservice.service.ts
+++ src/app/services/newsapiservice.service.ts
@@ -9,7 +9,7 @@ export class NewsapiserviceService {
 
   constructor(private _http: HttpClient) { }
 
-  newsApiUrl = "https://newsapi.org/v2/everything?q=Ukraine+Vladimir+Putin+Russia+war+invasion+evacuation&from=2022-05-01&sortBy=publishedAt&apiKey=86f4142b27b8423893fabc060d2eaea0";
+  newsApiUrl = "https://newsapi.org/v2/everything?q=Ukraine+Vladimir+Putin+Russia+war+invasion+evacuation&from=2022-05-22&sortBy=publishedAt&apiKey=86f4142b27b8423893fabc060d2eaea0";
 
   ukraineNews(): Observable<any> {
     return this._http.get(this.newsApiUrl);
